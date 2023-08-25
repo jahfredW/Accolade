12fed9b9286ec5a973c8c25a702b4eda75c23070
fredator <fred.gruwe@gmail.com>
2023-08-25 22:47:12 +0200

    gesdinet refesh token ok, et gestion du path dans le swagger. Resfresh token en bdd

--- project/src/OpenApi/OpenApiFactory.php
+++ project/src/OpenApi/OpenApiFactory.php
@@ -3,9 +3,12 @@
 namespace App\OpenApi;
 
 
+use ArrayObject;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\OpenApi\OpenApi;
 use ApiPlatform\OpenApi\Model\PathItem;
 use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\RequestBody;
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
 
 class OpenApiFactory implements OpenApiFactoryInterface
@@ -30,7 +33,33 @@ class OpenApiFactory implements OpenApiFactoryInterface
         }
 
        
-        $openApi->getPaths()->addPath('/ping', new PathItem(null, 'Ping', null, new Operation(null, [], [], 'Répond')));
+        $openApi->getPaths()->addPath('/ping', new PathItem(null, 'Ping', null, new Operation(null, ["ping"], [], 'Répond')));
+        $openApi->getPaths()->addPath('/api/token/refresh', new PathItem(null, 'Refresh_Token', null, null, null, new Operation(null, ['auth'], ['200' => [
+                        'description' => 'OK',
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    'type' => 'string',
+                                    'exemple' => 'edzedkzopekdopze',
+                                ]
+                            ]
+                        ]
+                    ]], null, null, null, null, new RequestBody(
+            required: false,
+                    content: new ArrayObject(
+                        [
+                            'application/json' => [
+                                'schema' => [
+                                    'type' => 'object',
+                                    'properties' => new ArrayObject(["refresh_token" => ['type' => 'string']]), // Aucune propriété
+                                    'additionalProperties' => false // Pas d'autres propriétés autorisées
+                                ]
+                            ]
+                        ]
+                    )
+                
+            
+        ),)));
         
         return $openApi;
     }
